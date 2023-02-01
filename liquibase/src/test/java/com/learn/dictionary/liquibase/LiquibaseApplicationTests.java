a47c08b6b9ee799d4277772499db5a90a90cf340
Oleksandr Kostenko <oleksandr.kostenko@outlook.com>
2023-02-01 10:31:44 +0200

    Added stub projects

---
+++ liquibase/src/test/java/com/learn/dictionary/liquibase/LiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.dictionary.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
